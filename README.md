9c391bd2e10e42e308f26d96ecbcc1c8b5267814
Thomas Nield <Thomas.Nield@wnco.com>
2018-01-16 12:23:54 -0600

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # okAlgo
-Idiomatic Kotlin extensions for ojAlgo
+
+
+Idiomatic Kotlin extensions for ojAlgo. 
+
+Currently work is focused on linear/integer optimization part of the API. Closures can flexibly be invoked wherever they are needed. 
+
+
+```kotlin 
+expressionsbasedmodel {
+
+    val v1 = variable(lower = 3, upper = 6)
+    val v2 = variable(lower = 10, upper = 12)
+
+    expression(weight = 1) {
+        set(v1, 1)
+        set(v2, 1)
+    }
+
+    maximise()
+
+    println("v1=${v1.value.toDouble()} v2=${v2.value.toDouble()}")
+}
+```
+
+```kotlin 
+val model = ExpressionsBasedModel()
+        
+val v1 = model.variable(lower = 3, upper = 6)
+val v2 = model.variable(lower = 10, upper = 12)
+
+model.expression(weight=1) {
+    set(v1, 1)
+    set(v2, 1)
+}
+
+model.maximise()
+
+println("v1=${v1.value.toDouble()} v2=${v2.value.toDouble()}")
+```
--- src/test/kotlin/org/nield/okalgo/OptimizationTest.kt
+++ src/test/kotlin/org/nield/okalgo/OptimizationTest.kt
@@ -1,6 +1,7 @@
 package org.nield.okalgo
 
 import org.junit.Test
+import org.ojalgo.optimisation.ExpressionsBasedModel
 
 
 class OptimizationTest {
@@ -8,20 +9,19 @@ class OptimizationTest {
     @Test
     fun expressionsBasedModelDSL() {
 
-        expressionsbasedmodel {
+        val model = ExpressionsBasedModel()
 
-            val v1 = variable(lower = 3, upper = 6, weight = 1)
-            val v2 = variable(lower = 10, upper = 12, weight = 1)
+        val v1 = model.variable(lower = 3, upper = 6)
+        val v2 = model.variable(lower = 10, upper = 12)
 
-            expression(weight = 1) {
-                set(v1, 1)
-                set(v2, 1)
-            }
-
-            maximise()
-
-            println("v1=${v1.value.toDouble()} v2=${v2.value.toDouble()}")
+        model.expression(weight=1) {
+            set(v1, 1)
+            set(v2, 1)
         }
+
+        model.maximise()
+
+        println("v1=${v1.value.toDouble()} v2=${v2.value.toDouble()}")
 
     }
 }
