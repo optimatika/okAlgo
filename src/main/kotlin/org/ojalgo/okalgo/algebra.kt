19e5797c62edb8757f58d7f3f5c69f236e461b20
Thomas Nield <thomasnield@live.com>
2018-12-19 13:06:34 -0600

    update to 47.0.0

--- src/main/kotlin/org/ojalgo/okalgo/algebra.kt
+++ src/main/kotlin/org/ojalgo/okalgo/algebra.kt
@@ -2,11 +2,7 @@ package org.ojalgo.okalgo
 
 import org.ojalgo.algebra.Operation
 import org.ojalgo.algebra.ScalarOperation
-import org.ojalgo.array.BigArray
-import org.ojalgo.array.BufferArray
-import org.ojalgo.array.ComplexArray
-import org.ojalgo.array.RationalArray
-import org.ojalgo.matrix.BasicMatrix
+import org.ojalgo.array.*
 import org.ojalgo.matrix.ComplexMatrix
 import org.ojalgo.matrix.PrimitiveMatrix
 import org.ojalgo.matrix.RationalMatrix
@@ -14,7 +10,6 @@ import org.ojalgo.scalar.ComplexNumber
 import org.ojalgo.scalar.Quaternion
 import org.ojalgo.scalar.RationalNumber
 import java.math.BigDecimal
-
 
 
 fun <T, N: Number> Sequence<T>.toPrimitiveMatrix(vararg selectors: (T) -> N): PrimitiveMatrix {
@@ -77,7 +72,6 @@ fun <T, N: Number> Iterable<T>.toRationalMatrix(vararg selectors: (T) -> N): Rat
     }
 }
 
-
 fun bigArrayOf(vararg values: Double) = BigArray.FACTORY.copy(*values)
 fun bigArrayOf(vararg values: ComplexNumber) = BigArray.FACTORY.copy(*values)
 fun bigArrayOf(vararg values: BigDecimal) = BigArray.FACTORY.copy(*values)
@@ -93,7 +87,8 @@ fun quanternionArrayOf(vararg values: Quaternion) = Quaternion.FACTORY.newArrayI
 
 fun rationalArrayOf(vararg values: RationalNumber) = RationalArray.FACTORY.copy(*values)
 
-/*
+
+
 fun vectorOf(vararg values: Int) = primitivematrix(values.count(), 1) {
     populate { row, col -> values[row.toInt()]  }
 }
@@ -109,28 +104,32 @@ fun vectorOf(vararg values: Long) = primitivematrix(values.count(), 1) {
 fun vectorOf(vararg values: BigDecimal) = rationalmatrix(values.count(), 1) {
     populate { row, col -> values[row.toInt()]  }
 }
-*/
 
 
-fun primitivematrix(rows: Int, cols: Int, op: (BasicMatrix.Builder<PrimitiveMatrix>.() -> Unit)? = null) =
-        PrimitiveMatrix.FACTORY.getBuilder(rows,cols).also {
+fun primitivematrix(rows: Int, cols: Int, op: (PrimitiveMatrix.DenseReceiver.() -> Unit)? = null) =
+        PrimitiveMatrix.FACTORY.makeDense(rows,cols).also {
             if (op != null) op(it)
         }.build()
 
 
-fun complexmatrix(rows: Int, cols: Int, op: (BasicMatrix.Builder<ComplexMatrix>.() -> Unit)? = null) =
-        ComplexMatrix.FACTORY.getBuilder(rows,cols).also {
+fun complexmatrix(rows: Int, cols: Int, op: (ComplexMatrix.DenseReceiver.() -> Unit)? = null) =
+        ComplexMatrix.FACTORY.makeDense(rows,cols).also {
             if (op != null) op(it)
         }.build()
 
-fun rationalmatrix(rows: Int, cols: Int, op: (BasicMatrix.Builder<RationalMatrix>.() -> Unit)? = null) =
-        RationalMatrix.FACTORY.getBuilder(rows,cols).also {
+fun rationalmatrix(rows: Int, cols: Int, op: (RationalMatrix.DenseReceiver.() -> Unit)? = null) =
+        RationalMatrix.FACTORY.makeDense(rows,cols).also {
             if (op != null) op(it)
         }.build()
 
-fun <I: BasicMatrix> BasicMatrix.Builder<I>.populate(op: (Long,Long) -> Number) =
+fun PrimitiveMatrix.DenseReceiver.populate(op: (Long,Long) -> Number) =
         loopAll { row, col -> set(row, col, op(row,col))  }
 
+fun ComplexMatrix.DenseReceiver.populate(op: (Long,Long) -> Number) =
+        loopAll { row, col -> set(row, col, op(row,col))  }
+
+fun RationalMatrix.DenseReceiver.populate(op: (Long,Long) -> Number) =
+        loopAll { row, col -> set(row, col, op(row,col))  }
 
 operator fun <T> Operation.Addition<T>.plus(t: T) = add(t)
 operator fun <T> Operation.Division<T>.div(t: T) = divide(t)
@@ -142,5 +141,12 @@ operator fun <T, N: Number> ScalarOperation.Division<T,N>.div(number: N) = divid
 operator fun <T, N: Number> ScalarOperation.Multiplication<T,N>.times(number: N) = multiply(number)
 operator fun <T, N: Number> ScalarOperation.Subtraction<T,N>.minus(number: N) = subtract(number)
 
+fun Double.toComplex() = ComplexNumber.FACTORY.cast(this)
+fun Double.toRational() = RationalNumber.FACTORY.cast(this)
+fun <N: Number> N.toComplex() = ComplexNumber.FACTORY.cast(this)
+fun <N: Number> N.toRational() = RationalNumber.FACTORY.cast(this)
 
-
+fun Double.toComplexScalar() = ComplexNumber.FACTORY.convert(this)
+fun Double.toRationalScalar() = RationalNumber.FACTORY.convert(this)
+fun <N: Number> N.toComplexScalar() = ComplexNumber.FACTORY.convert(this)
+fun <N: Number> N.toRationalScalar() = RationalNumber.FACTORY.convert(this)
